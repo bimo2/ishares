49ff4e1e5c13b43ab949f47144b714643d5d6be9
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2024-03-06 22:47:47 -0500

    parse all xlsx files

--- build.sbt
+++ build.sbt
@@ -1,1 +1,3 @@
 scalaVersion := "3.4.0"
+
+libraryDependencies += "org.apache.poi" % "poi-ooxml" % "5.2.5"
--- ishares.scala
+++ ishares.scala
@@ -1,1 +1,26 @@
-@main def script(args: String*): Unit = { }
+import java.io.File
+import org.apache.poi.ss.usermodel.WorkbookFactory
+
+def sql(xlsx: File): Unit = {
+  println(s"generate sql from: ${xlsx.getName}")
+
+  val workbook = WorkbookFactory.create(xlsx)
+
+  for (index <- 0 until workbook.getNumberOfSheets) {
+    val sheet = workbook.getSheetAt(index)
+
+    println(s"sheet: ${sheet.getSheetName}")
+  }
+
+  workbook.close()
+}
+
+@main def script(args: String*): Unit = {
+  val xlsx = new File("xlsx")
+
+  if (!xlsx.isDirectory) return
+
+  val files = xlsx.listFiles.filter(_.isFile)
+
+  files.foreach(sql)
+}
