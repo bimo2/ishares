ac763367861a58a8b8d17f19a4d2b4c847c6d33a
Bimal Bhagrath <48932917+bimo2@users.noreply.github.com>
2024-03-07 00:29:21 -0500

    create ishares schema.sql

--- ishares.scala
+++ ishares.scala
@@ -1,5 +1,74 @@
-import java.io.File
-import org.apache.poi.ss.usermodel.WorkbookFactory
+import java.io.{File, PrintWriter}
+import org.apache.poi.ss.usermodel.{Sheet, WorkbookFactory}
+
+val schemaSQL = """
+DROP USER IF EXISTS device;
+DROP DATABASE IF EXISTS ishares;
+CREATE DATABASE ishares;
+
+CREATE TABLE etfs (
+  id VARCHAR(10) PRIMARY KEY,
+  issuer VARCHAR(255),
+  name VARCHAR(255),
+  report_date DATE
+);
+
+CREATE TABLE assets (
+  id VARCHAR(10) PRIMARY KEY,
+  name VARCHAR(255),
+  sector VARCHAR(255),
+  class VARCHAR(255),
+  location VARCHAR(255),
+  exchange VARCHAR(255)
+);
+
+CREATE TABLE quotes (
+  etf_id VARCHAR(10) NOT NULL,
+  date DATE NOT NULL,
+  nav_per_share DECIMAL(18, 2),
+  shares DECIMAL(18, 4),
+  PRIMARY KEY (etf_id, date),
+  FOREIGN KEY (etf_id) REFERENCES etfs(id)
+);
+
+CREATE TABLE holdings (
+  etf_id VARCHAR(10) NOT NULL,
+  asset_id VARCHAR(10) NOT NULL,
+  market_value DECIMAL(18, 2),
+  weight FLOAT,
+  notional_value DECIMAL(18, 2),
+  shares DECIMAL(18, 4),
+  PRIMARY KEY (etf_id, asset_id),
+  FOREIGN KEY (etf_id) REFERENCES etfs(id),
+  FOREIGN KEY (asset_id) REFERENCES assets(id)
+);
+
+CREATE TABLE dividends (
+  etf_id VARCHAR(10) NOT NULL,
+  record_date DATE NOT NULL,
+  ex_date DATE,
+  payable_date DATE,
+  value DECIMAL(18, 2) NOT NULL,
+  PRIMARY KEY (etf_id, record_date),
+  FOREIGN KEY (etf_id) REFERENCES etfs(id)
+);
+
+CREATE USER 'device'@'%' IDENTIFIED BY 'CL0UD5Q1';
+GRANT ALL PRIVILEGES ON ishares.* TO 'device'@'%';
+FLUSH PRIVILEGES;
+""".stripMargin.trim
+
+def holdingsSQL(sheet: Sheet): Unit = {
+  println(s"parse holdings")
+}
+
+def historicalSQL(sheet: Sheet): Unit = {
+  println(s"parse historical")
+}
+
+def distributionsSQL(sheet: Sheet): Unit = {
+  println(s"parse distributions")
+}
 
 def sql(xlsx: File): Unit = {
   println(s"generate sql from: ${xlsx.getName}")
@@ -9,18 +78,42 @@ def sql(xlsx: File): Unit = {
   for (index <- 0 until workbook.getNumberOfSheets) {
     val sheet = workbook.getSheetAt(index)
 
-    println(s"sheet: ${sheet.getSheetName}")
+    sheet.getSheetName match {
+      case "Holdings" => holdingsSQL(sheet)
+      case "Historical" => historicalSQL(sheet)
+      case "Distributions" => distributionsSQL(sheet)
+      case _ => // do nothing
+    }
   }
 
   workbook.close()
 }
 
+def write(path: String, content: String): Unit = {
+  val file = new File(path)
+  val directory = file.getParentFile
+
+  if (!directory.exists()) directory.mkdirs()
+
+  file.createNewFile()
+
+  val writer = new PrintWriter(file)
+
+  try {
+    writer.println(content)
+  } finally {
+    writer.close()
+  }
+}
+
 @main def script(args: String*): Unit = {
-  val xlsx = new File("xlsx")
+  write("sql/schema.sql", schemaSQL)
 
-  if (!xlsx.isDirectory) return
+  val xlsxPath = new File("xlsx")
 
-  val files = xlsx.listFiles.filter(_.isFile)
+  if (!xlsxPath.isDirectory) return
+
+  val files = xlsxPath.listFiles.filter(_.isFile)
 
   files.foreach(sql)
 }
